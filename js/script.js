de9aed787838026a198c27e14adfeb3815fb0e07
Rafa Sequeira <rsg_2008@hotmail.es>
2018-01-11 17:27:45 -0600

    mejorar en welcome section y tours section, bike section en progreso

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-function display_menu() {
-    var x = document.getElementById("myTopnav");
+function display_menu() { //funcion para despleglegar el menu, inyecta la clase topnav responsive
+    let x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
@@ -7,10 +7,29 @@ function display_menu() {
     }
 }
 
-$(document).ready(function () {
+/*$(document).ready(function () {
     $("#menuLogo").click(function () {
         $('#h1-tours').addClass('wow fadeInLeft');
 
     });
-});
+});*/
 
+
+// funciones del slider
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    let i;
+    let x = document.getElementsByClassName("mySlides");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
